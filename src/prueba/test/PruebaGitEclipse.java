ab16edf4d2e9005f38b092fd238597021a603090
Montse <Montse@PC-LEN-Montse>
2020-03-28 16:58:57 +0100

    Eliminado saludo a Ana

--- src/prueba/test/PruebaGitEclipse.java
+++ src/prueba/test/PruebaGitEclipse.java
@@ -7,7 +7,6 @@ public class PruebaGitEclipse {
 	public static void main(String[] args) {
 		Saludo saludo = new Saludo();
 		System.out.println(saludo.saludar("Juan"));
-		System.out.println(saludo.saludar("Ana"));
 		System.out.println(saludo.saludar("Luis"));
 		System.out.println(saludo.saludar("Elena"));
 	}
